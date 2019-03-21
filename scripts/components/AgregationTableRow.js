d07a406798006d21db0464e68adb5a9f1b28cc99
David Bruant <bruant.d@gmail.com>
2019-03-21 11:38:42 +0100

    Rough feedback on focused formula

--- scripts/components/AgregationTableRow.js
+++ scripts/components/AgregationTableRow.js
@@ -1,17 +1,59 @@
-import {h} from 'preact'
+import {h, Component} from 'preact'
 import {sum} from 'd3-array'
 
-export default function({id, name, formula, rows, onNameChange, onFormulaChange}){
-    return html`
-        <tr key=${id}>
-            <td>
-                <input type="text" value=${name} onInput=${onNameChange}/>
-            </td>
-            <td>
-                <input type="text" value=${formula} onInput=${onFormulaChange}/>
-            </td>
-            <td>${rows.size}</td>
-            <td>${sum(rows.toJS().map(r => r['MtReal'])).toFixed(2)+'€'}</td>
-        </tr>
-    `
+export default class AgregationTableRow extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {focused: false};
+    }
+
+    render({id, name, formula, rows, onNameChange, onFormulaChange}, {focused}){
+        console.log('focused', focused)
+
+        return html`
+            <tr key=${id}>
+                <td>
+                    <input type="text" value=${name} onInput=${onNameChange}/>
+                </td>
+                <td>
+                    <input type="text" value=${formula} onInput=${onFormulaChange} 
+                        onFocus=${() => this.setState({focused: true})}
+                        onBlur=${() => this.setState({focused: false})}
+                        />
+                    ${
+                        focused && rows.size >= 1 ?
+                            html`
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            ${['RD', 'FI', 'Fonction', 'Nature', 'Montant'].map(s => html`<th>${s}</th>`)}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        ${
+                                            rows.toArray().map(r => {
+                                                return html`
+                                                    <tr>
+                                                        <td>${r['CodRD']}</td>
+                                                        <td>${r['FI']}</td>
+                                                        <td>${r['Fonction']}</td>
+                                                        <td>${r['Nature']}</td>
+                                                        <td>${r['MtReal'].toFixed(2)+'€'}</td>
+                                                    </tr>
+                                                `
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
+                            ` : 
+                            undefined
+
+                    }
+                </td>
+                <td>${rows.size}</td>
+                <td>${sum(rows.toJS().map(r => r['MtReal'])).toFixed(2)+'€'}</td>
+            </tr>
+        `
+    }
 }
