39e4d11dcbd0b3b01d28e13f00d8eb0192c8ec86
Thomas Parisot <oncletom@users.noreply.github.com>
2019-12-12 19:13:06 +0100

    Aggrégation Montreuil V12

--- scripts/main.js
+++ scripts/main.js
@@ -32,7 +32,7 @@ if(isMontreuil){
 
 	// Download Montreuil "Open data nomenclature" CSV and transform it to formulas
 	Promise.all([
-		csv('./data/agregation-Montreuil-v7.csv'),
+		csv('./data/agregation-Montreuil-v12.csv'),
 		docBudgP
 	])
 	.then(([csvData, docBudg]) => montreuilCVSToAgregationFormulas(csvData, [docBudg]))
